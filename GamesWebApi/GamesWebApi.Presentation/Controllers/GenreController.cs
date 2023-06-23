3405e741602a5dfb60250af9c328c45a94394b9e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-23 17:23:11 -0300

    add GenreController

---
+++ GamesWebApi/GamesWebApi.Presentation/Controllers/GenreController.cs
@@ -0,0 +1,49 @@
+using Application.Exception;
+using Application.Service.Contracts;
+using Domain.Entity;
+using FluentValidation;
+using FluentValidation.Results;
+using GamesWebApi.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesWebApi.Controllers;
+
+[ApiController]
+public class GenreController : ControllerBase
+{
+    private readonly IGenreService _service;
+
+    public GenreController(IGenreService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost(ApiRoutes.GenreRoutes.Create)]
+    public async Task<IActionResult> CreateGenre([FromServices] IValidator<CreateGenreDTO> validator, CreateGenreDTO dto)
+    {
+        ValidationResult result = await validator.ValidateAsync(dto);
+
+        if (!result.IsValid)
+        {
+            return BadRequest(result.Errors);
+        }
+        
+        try
+        {
+            var genre = new Genre(dto.Name);
+            
+            var genreSaved = await _service.CreateGenre(genre);
+
+            return Created(ApiRoutes.GenreRoutes.Create, genreSaved);
+        }
+        catch (GenreAlreadyExistsException e)
+        {
+            var errorsDto = new ErrorResponseDTO();
+            errorsDto.Errors.Add(e.Message);
+            
+            return BadRequest(errorsDto);
+        }
+
+        return StatusCode(500);
+    }
+}
