7daa35fa63146f146c67cb92a1bd85c295d2de2d
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-23 15:56:15 -0300

    run migration for GenreMap

---
+++ GamesWebApi/GamesWebApi.Presentation/Migrations/20230623185222_Genre.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamesWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Genre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Id", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
