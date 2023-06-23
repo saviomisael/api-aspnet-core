30f68dd1a26972cbdd4dc46c33bd7a64276fce99
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-23 14:46:52 -0300

    add connection string for sql server

--- GamesWebApi/GamesWebApi.Presentation/Program.cs
+++ GamesWebApi/GamesWebApi.Presentation/Program.cs
@@ -1,6 +1,20 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("GamesWebApi"), sqpOptions =>
+    {
+        sqpOptions.EnableRetryOnFailure(
+            maxRetryCount: 10,
+            maxRetryDelay: TimeSpan.FromSeconds(30),
+            errorNumbersToAdd: null
+        );
+    });
+});
 
 var app = builder.Build();
 
