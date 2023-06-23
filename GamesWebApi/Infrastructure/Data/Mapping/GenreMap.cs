0d33737f125df5ec6486e8ee919273fbfddcbdcb
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-23 15:57:12 -0300

    add migrations config

--- GamesWebApi/Infrastructure/Data/Mapping/GenreMap.cs
+++ GamesWebApi/Infrastructure/Data/Mapping/GenreMap.cs
@@ -9,6 +9,7 @@ public class GenreMap : IEntityTypeConfiguration<Genre>
     public void Configure(EntityTypeBuilder<Genre> builder)
     {
         builder.ToTable("Genres");
+        builder.Property(g => g.Id).HasMaxLength(36);
         builder.HasKey(g => g.Id).HasName("Id");
         builder.Property(g => g.Name).HasMaxLength(255).IsRequired();
     }
