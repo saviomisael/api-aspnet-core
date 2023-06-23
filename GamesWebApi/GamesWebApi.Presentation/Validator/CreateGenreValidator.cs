d3d91a48bb037ac851f5ad8ee755d1e67a69da72
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-23 17:21:50 -0300

    add CreateGenreValidator

---
+++ GamesWebApi/GamesWebApi.Presentation/Validator/CreateGenreValidator.cs
@@ -0,0 +1,12 @@
+using FluentValidation;
+using GamesWebApi.DTO;
+
+namespace GamesWebApi.Validator;
+
+public class CreateGenreValidator : AbstractValidator<CreateGenreDTO>
+{
+    public CreateGenreValidator()
+    {
+        RuleFor(x => x.Name).Length(3, 255).WithMessage("Genre must have a name with at least 3 characters and a maximum of 255 characters.");
+    }
+}
