3358148512d31c1eecf147ffee27e680001a7b30
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-29 15:15:02 -0300

    pass test CreatePlatform_ShouldReturnBadRequest_WhenPlatformAlreadyExists

--- GamesWebApi/GamesWebApi.Tests/Functional/PlatformControllerTests.cs
+++ GamesWebApi/GamesWebApi.Tests/Functional/PlatformControllerTests.cs
@@ -1,7 +1,9 @@
 using System.Net;
 using System.Net.Http;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entity;
 using FluentAssertions;
 using GamesWebApi.DTO;
 using GamesWebApi.V1;
@@ -39,7 +41,29 @@ public class PlatformControllerTests : IAsyncLifetime
 
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
-    
+
+    [Fact]
+    public async void CreatePlatform_ShouldReturnBadRequest_WhenPlatformAlreadyExists()
+    {
+        _context.Platforms.Add(new Platform("xbox"));
+        await _context.SaveChangesAsync();
+
+        var client = _factory.CreateClient();
+
+        var dto = new CreatePlatformDto { Name = "xbox" };
+
+        var request = new StringContent(JsonConvert.SerializeObject(dto), Encoding.UTF8,
+            MediaTypeNames.Application.Json);
+
+        var response = await client.PostAsync(ApiRoutes.PlatformRoutes.Create, request);
+        
+        var body = response.Content.ReadAsStringAsync().Result;
+        var errorsFromBody = JsonConvert.DeserializeObject<ErrorResponseDto>(body);
+
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        errorsFromBody.Errors.Contains("Platform xbox already exists.").Should().BeTrue();
+    }
+
     public async Task InitializeAsync()
     {
         await _context.Database.ExecuteSqlRawAsync("DELETE FROM Platforms");
