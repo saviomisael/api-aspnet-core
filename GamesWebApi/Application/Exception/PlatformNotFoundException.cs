8535ed72e48cbf059932df84965d970f98305a00
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-29 16:16:42 -0300

    add PlatformNotFoundException

---
+++ GamesWebApi/Application/Exception/PlatformNotFoundException.cs
@@ -0,0 +1,9 @@
+namespace Application.Exception;
+
+public class PlatformNotFoundException : System.Exception
+{
+    public PlatformNotFoundException(string platform) : base($"Platform {platform} not found.")
+    {
+        
+    }
+}
