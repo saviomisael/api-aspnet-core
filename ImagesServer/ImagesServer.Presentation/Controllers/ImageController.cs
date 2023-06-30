17f3549e10011b675501756d3d3472310035ab0a
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-30 20:29:55 -0300

    add swagger docs

--- ImagesServer/ImagesServer.Presentation/Controllers/ImageController.cs
+++ ImagesServer/ImagesServer.Presentation/Controllers/ImageController.cs
@@ -60,8 +60,16 @@ public class ImageController : ControllerBase
         return Created(ApiRoutes.Images.CreateImage, response);
     }
 
+    /// <summary>
+    /// Return an image from database.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns>Return an image from database.</returns>
+    /// <response code="404">Image not found.</response>
+    /// <response code="200">Return image requested.</response>
     [Produces(MediaTypeNames.Image.Jpeg, "image/jpg", "image/png", MediaTypeNames.Application.Json)]
     [ProducesResponseType(typeof(ErrorsDto), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(FileContentResult), StatusCodes.Status200OK)]
     [HttpGet(ApiRoutes.Images.GetImage)]
     public async Task<IActionResult> GetImage(string name)
     {
