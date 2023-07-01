28258d5925a27c02c28bc49917c542a8b5fd9e38
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-01 09:07:47 -0300

    update ApplicationExtensions

--- GamesWebApi/GamesWebApi.Presentation/IoC/ApplicationExtensions.cs
+++ GamesWebApi/GamesWebApi.Presentation/IoC/ApplicationExtensions.cs
@@ -10,5 +10,6 @@ public static class ApplicationExtensions
         service.AddScoped<IGenreService, GenreService>();
         service.AddScoped<IPlatformService, PlatformService>();
         service.AddScoped<IAgeRatingService, AgeRatingService>();
+        service.AddScoped<IGameService, GameService>();
     }
 }
