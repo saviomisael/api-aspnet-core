34ab4be8f236723774cbd8a5ed1238e42bc704b7
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-01 09:12:53 -0300

    add CreateGameDto

---
+++ GamesWebApi/GamesWebApi.Presentation/DTO/CreateGameDto.cs
@@ -0,0 +1,13 @@
+namespace GamesWebApi.DTO;
+
+public class CreateGameDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string UrlImage { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public ICollection<string> Genres { get; set; } = new List<string>();
+    public ICollection<string> Platforms { get; set; } = new List<string>();
+    public IFormFile Image { get; set; } = null!;
+}
