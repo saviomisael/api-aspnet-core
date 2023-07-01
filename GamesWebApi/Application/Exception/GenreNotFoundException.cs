56b82adbe2ddae1b0951863804337db33c9e4ef5
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-01 08:22:04 -0300

    refactor GenreNotFoundException

--- GamesWebApi/Application/Exception/GenreNotFoundException.cs
+++ GamesWebApi/Application/Exception/GenreNotFoundException.cs
@@ -5,4 +5,9 @@ public class GenreNotFoundException : System.Exception
     public GenreNotFoundException(string genre) : base($"Genre {genre} not found.")
     {
     }
+
+    public GenreNotFoundException() : base("Genre not found.")
+    {
+        
+    }
 }
