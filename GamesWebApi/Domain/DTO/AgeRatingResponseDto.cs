c0725a187fac1948c9d7448d3270b75b240d20fd
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-02 17:01:53 -0300

    add AgeRatingResponseDto

---
+++ GamesWebApi/Domain/DTO/AgeRatingResponseDto.cs
@@ -0,0 +1,8 @@
+namespace Domain.DTO;
+
+public class AgeRatingResponseDto
+{
+    public string Id { get; set; }
+    public string Age { get; set; }
+    public string Description { get; set; }
+}
