8334f0458424b7edc5842bd4cfb5ab1bdbdb87af
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-02 17:28:26 -0300

    add GameResponseDto

---
+++ GamesWebApi/Domain/DTO/GameResponseDto.cs
@@ -0,0 +1,14 @@
+namespace Domain.DTO;
+
+public class GameResponseDto
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string UrlImage { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public AgeRatingResponseDto AgeRating { get; set; }
+    public ICollection<GenreResponseDto> Genres { get; set; }
+    public ICollection<PlatformResponseDto> Platforms { get; set; }
+}
