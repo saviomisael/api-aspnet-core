ee83af2aa5eac51968c43312c3c52308af95984f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-03 11:49:47 -0300

    add IsImageSizeLessThanOrEqualTo and IsImageTypeSupported for ValidatorHelper

--- GamesWebApi/GamesWebApi.Presentation/Helpers/ValidatorHelper.cs
+++ GamesWebApi/GamesWebApi.Presentation/Helpers/ValidatorHelper.cs
@@ -7,4 +7,14 @@ public static class ValidatorHelper
     public static bool NotHasDuplicates<T>(this ICollection<T> collection) => collection.Count == collection.GroupBy(x => x).Count();
 
     public static bool IsAValidDate(this string date) => DateTime.TryParse(date, out _);
+
+    public static bool IsImageSizeLessThanOrEqualTo(long size, long x)
+    {
+        return size <= x;
+    }
+
+    public static bool IsImageTypeSupported(string imageType, string[] validImageTypes)
+    {
+        return validImageTypes.Contains(imageType);
+    }
 }
