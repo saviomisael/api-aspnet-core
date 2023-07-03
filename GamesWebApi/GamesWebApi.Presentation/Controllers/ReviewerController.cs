9e8cbcfc283ae0fd71c493ad4f5476137aead317
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-03 19:29:17 -0300

    add swagger docs for CreateAccount route

--- GamesWebApi/GamesWebApi.Presentation/Controllers/ReviewerController.cs
+++ GamesWebApi/GamesWebApi.Presentation/Controllers/ReviewerController.cs
@@ -1,5 +1,6 @@
 using System.Net.Mime;
 using Application.Exception;
+using Domain.DTO;
 using Domain.Entity;
 using Domain.Service;
 using FluentValidation;
@@ -20,6 +21,15 @@ public class ReviewerController : ControllerBase
         _service = service;
     }
 
+    /// <summary>
+    /// Create a reviewer account.
+    /// </summary>
+    /// <param name="dto"></param>
+    /// <returns>The token to be used in authorized routes.</returns>
+    /// <response code="400">Returns the errors in the request.</response>
+    /// <response code="201">Returns the token, expiration time and username from newly account created.</response>
+    [ProducesResponseType(typeof(ErrorResponseDto), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ReviewerTokenDto), StatusCodes.Status201Created)]
     [HttpPost(ApiRoutes.ReviewersRoutes.CreateAccount)]
     public async Task<IActionResult> CreateAccount([FromServices] IValidator<CreateAccountDto> validator,
         CreateAccountDto dto)
