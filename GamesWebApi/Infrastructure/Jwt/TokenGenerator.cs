7e35bc6c23c383842bef5af9bfbd698465b9e325
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-03 15:43:00 -0300

    add TokenGenerator

---
+++ GamesWebApi/Infrastructure/Jwt/TokenGenerator.cs
@@ -0,0 +1,40 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Domain.DTO;
+using Infrastructure.Jwt.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Jwt;
+
+public class TokenGenerator
+{
+    private readonly JwtOptions _options;
+
+    public TokenGenerator(JwtOptions options)
+    {
+        _options = options;
+    }
+    
+    public ReviewerTokenDto GenerateToken(string reviewerId, string reviewerUsername)
+    {
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, reviewerId), new Claim("UserName", reviewerUsername),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Key));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var expiration = DateTime.UtcNow.AddMinutes(30);
+
+        var token = new JwtSecurityToken(_options.Issuer, _options.Audience, claims, DateTime.UtcNow, expiration, credentials);
+
+        return new ReviewerTokenDto
+        {
+            Token = new JwtSecurityTokenHandler().WriteToken(token),
+            Expiration = expiration,
+            Username = reviewerUsername
+        };
+    }
+}
