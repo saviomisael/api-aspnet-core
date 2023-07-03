005c89ed809d794072123fa0ed9894b40ca6eb40
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-03 15:17:12 -0300

    add migration for Identity

--- GamesWebApi/Infrastructure/Data/AppDbContext.cs
+++ GamesWebApi/Infrastructure/Data/AppDbContext.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
-public class AppDbContext : IdentityDbContext
+public class AppDbContext : IdentityDbContext<Reviewer>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 
@@ -15,6 +15,7 @@ public class AppDbContext : IdentityDbContext
         modelBuilder.ApplyConfiguration(new PlatformMap());
         modelBuilder.ApplyConfiguration(new AgeRatingMap());
         modelBuilder.ApplyConfiguration(new GameMap());
+        base.OnModelCreating(modelBuilder);
     }
 
     public virtual DbSet<Genre> Genres { get; set; }
