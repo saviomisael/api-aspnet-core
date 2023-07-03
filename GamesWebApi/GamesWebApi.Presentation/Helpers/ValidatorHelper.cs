39b0633899990309cb6ccf921647cabf038fa0d4
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-03 10:34:30 -0300

    refactor ValidatorHelper

--- GamesWebApi/GamesWebApi.Presentation/Helpers/ValidatorHelper.cs
+++ GamesWebApi/GamesWebApi.Presentation/Helpers/ValidatorHelper.cs
@@ -1,21 +1,10 @@
-using System.Collections;
-
 namespace GamesWebApi.Helpers;
 
 public static class ValidatorHelper
 {
-    public static bool IsGreaterThanXAndLessThanY<T>(this ICollection<T> collection, int x, int y)
-    {
-        return collection.Count > x && collection.Count < y;
-    }
+    public static bool IsGreaterThanXAndLessThanY<T>(this ICollection<T> collection, int x, int y) => collection.Count > x && collection.Count < y;
 
-    public static bool NotHasDuplicates<T>(this ICollection<T> collection)
-    {
-        return collection.Count == collection.GroupBy(x => x).Count();
-    }
+    public static bool NotHasDuplicates<T>(this ICollection<T> collection) => collection.Count == collection.GroupBy(x => x).Count();
 
-    public static bool IsAValidDate(this string date)
-    {
-        return DateTime.TryParse(date, out _);
-    }
+    public static bool IsAValidDate(this string date) => DateTime.TryParse(date, out _);
 }
