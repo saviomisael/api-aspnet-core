1e2b243a893d2be8cb78d1ee4d10dfbc76d0f999
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-03 16:29:55 -0300

    fix CreateAccountValidator message

--- GamesWebApi/GamesWebApi.Presentation/Validator/CreateAccountValidator.cs
+++ GamesWebApi/GamesWebApi.Presentation/Validator/CreateAccountValidator.cs
@@ -10,7 +10,7 @@ public class CreateAccountValidator : AbstractValidator<CreateAccountDto>
         RuleFor(x => x.UserName).MinimumLength(3).MaximumLength(255)
             .WithMessage("UserName length must be between 3 and 255 characters.");
         RuleFor(x => x.Password).Equal(x => x.ConfirmPassword)
-            .WithMessage("Password and ConfirmPassword must the same.");
+            .WithMessage("Password and ConfirmPassword must be the same.");
         RuleFor(x => x.Email).EmailAddress().WithMessage("Invalid email format.");
     }
 }
