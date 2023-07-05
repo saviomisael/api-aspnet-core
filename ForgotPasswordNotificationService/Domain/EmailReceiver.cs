b52243d8d49cd84ea56ff84a40d20c061e20366b
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-05 16:25:41 -0300

    add EmailReceiver

---
+++ ForgotPasswordNotificationService/Domain/EmailReceiver.cs
@@ -0,0 +1,8 @@
+namespace Domain;
+
+public class EmailReceiver
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string RandomPassword { get; set; }
+}
