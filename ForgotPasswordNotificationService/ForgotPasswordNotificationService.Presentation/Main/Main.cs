2c86bf5b9ee5730281d87315d8b7bb92f67dcc62
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-05 16:38:51 -0300

    refactor Main

--- ForgotPasswordNotificationService/ForgotPasswordNotificationService.Presentation/Main/Main.cs
+++ ForgotPasswordNotificationService/ForgotPasswordNotificationService.Presentation/Main/Main.cs
@@ -1,6 +1,11 @@
+using Microsoft.Extensions.Hosting;
+
 namespace ForgotPasswordNotificationService.Main;
 
-public class Main
+public class Main : BackgroundService
 {
-    
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;
+    }
 }
