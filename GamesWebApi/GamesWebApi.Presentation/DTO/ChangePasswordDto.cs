04e8e49b8ed2a29385f18aa6c6b1db8e3dd78241
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-05 15:31:11 -0300

    refactor ChangePasswordDto

--- GamesWebApi/GamesWebApi.Presentation/DTO/ChangePasswordDto.cs
+++ GamesWebApi/GamesWebApi.Presentation/DTO/ChangePasswordDto.cs
@@ -4,4 +4,5 @@ public class ChangePasswordDto
 {
     public string CurrentPassword { get; set; }
     public string NewPassword { get; set; }
+    public string ConfirmPassword { get; set; }
 }
--- GamesWebApi/GamesWebApi.Presentation/Validator/ChangePasswordValidator.cs
+++ GamesWebApi/GamesWebApi.Presentation/Validator/ChangePasswordValidator.cs
@@ -9,5 +9,7 @@ public class ChangePasswordValidator : AbstractValidator<ChangePasswordDto>
     {
         RuleFor(x => x.NewPassword).NotEqual(x => x.CurrentPassword)
             .WithMessage("The new password must be different from the current password.");
+        RuleFor(x => x.NewPassword).Equal(x => x.ConfirmPassword)
+            .WithMessage("The new password and confirm password must be the equal.");
     }
 }
