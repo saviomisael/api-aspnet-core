2989133f5638711aac20c784e07d6aa3f941a85e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-05 11:37:31 -0300

    add GmailClient

---
+++ ChangePasswordNotificationService/Infrastructure/EmailSender/GmailClient.cs
@@ -0,0 +1,23 @@
+using System.Net;
+using System.Net.Mail;
+using Infrastructure.Options;
+
+namespace Infrastructure.EmailSender;
+
+public class GmailClient
+{
+    private GmailOptions _options;
+    
+    public GmailClient(GmailOptions options)
+    {
+        _options = options;
+    }
+
+    public async Task SendEmail(MailMessage mail)
+    {
+        using var smtp = new SmtpClient("smtp.gmail.com", 587);
+        smtp.Credentials = new NetworkCredential(_options.SenderEmail, _options.SenderPassword);
+        smtp.EnableSsl = true;
+        await smtp.SendMailAsync(mail);
+    }
+}
