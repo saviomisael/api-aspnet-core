0edbed9d62e7d0de527f37d7d77557ffbde22e0b
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-05 13:31:04 -0300

    add RabbitMqClient

---
+++ ChangePasswordNotificationService/Infrastructure/RabbitMQ/RabbitMqClient.cs
@@ -0,0 +1,52 @@
+using System.Text;
+using Domain;
+using Domain.EmailSender.Services;
+using Domain.Entity;
+using Infrastructure.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Infrastructure.RabbitMQ;
+
+public class RabbitMqClient
+{
+    private readonly ISendEmailService _sendEmailService;
+    private readonly ConnectionFactory _connectionFactory;
+
+    public RabbitMqClient(ISendEmailService service, RabbitMqOptions options)
+    {
+        _sendEmailService = service;
+        _connectionFactory = new ConnectionFactory
+        {
+            HostName = options.HostName, UserName = options.UserName, Password = options.Password
+        };
+    }
+
+    public void ConnectChangePasswordEmailQueue()
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare("change_password_notification", false, false, false, null);
+        
+        Console.WriteLine("Waiting for messages.");
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            var emailReceiver = JsonConvert.DeserializeObject<EmailReceiver>(message);
+
+            if (emailReceiver is null) return;
+
+            _sendEmailService.SendEmail(emailReceiver.Email, emailReceiver.UserName);
+            Console.WriteLine($"Send email notification for {emailReceiver.Email}.");
+        };
+
+        channel.BasicConsume("change_password_notification", true, consumer);
+    }
+}
