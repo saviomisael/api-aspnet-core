90ba327e9ff8e7892d3f714a7c878b330544272f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-05 10:20:24 -0300

    add initial project for ChangePasswordNotificationService

---
+++ ChangePasswordNotificationService/ChangePasswordNotificationService.Presentation/Main/Main.cs
@@ -0,0 +1,12 @@
+namespace ChangePasswordNotificationService.Main;
+
+public class Main
+{
+    public void Run()
+    {
+        Console.WriteLine("App is running!");
+        
+        Console.WriteLine("Click enter to exit from application.");
+        Console.ReadLine();
+    }
+}
---
+++ ChangePasswordNotificationService/ChangePasswordNotificationService.Presentation/Program.cs
@@ -0,0 +1,15 @@
+﻿using ChangePasswordNotificationService.Main;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddTransient<Main>();
+    })
+    .Build();
+
+using var serviceScope = host.Services.CreateScope();
+var provider = serviceScope.ServiceProvider;
+var main = provider.GetRequiredService<Main>();
+main.Run();
