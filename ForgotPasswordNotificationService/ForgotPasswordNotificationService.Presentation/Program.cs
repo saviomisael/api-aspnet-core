c2409dd2e2cb0dddbbfb8891601c8e149d4e536e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-07-05 16:38:37 -0300

    refactor Program

--- ForgotPasswordNotificationService/ForgotPasswordNotificationService.Presentation/Program.cs
+++ ForgotPasswordNotificationService/ForgotPasswordNotificationService.Presentation/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using ForgotPasswordNotificationService.IoC;
+using ForgotPasswordNotificationService.Main;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location))
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddSingletonOptions(config);
+        services.AddDependencies();
+        services.AddHostedService<Main>();
+    });
+
+var app = host.Build();
+
+await app.RunAsync();
